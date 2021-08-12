f5455ec7d7c527282ae0724787d11b6a530d6205
MaxBotvinev <max-bot@outlook.com>
2021-08-12 17:23:16 +0300

    Menu. Game Flow. Confirm Pop-Up.

--- Source/BEAR/BEARGameMode.cpp
+++ Source/BEAR/BEARGameMode.cpp
@@ -1,15 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "BEARGameMode.h"
-#include "BEARCharacter.h"
-#include "UObject/ConstructorHelpers.h"
 
 ABEARGameMode::ABEARGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/BEAR/BEAR_BP"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
